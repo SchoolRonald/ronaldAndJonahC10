4cce58563cf9acb02ce2473709a5c75c686458d4
Jonah Reynolds <jonahmr907@gmail.com>
2025-10-01 14:49:51 -0600

    First commit with initial classes

---
+++ src/ScoreTrakker.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+
+public class ScoreTrakker {
+	private ArrayList<Student> students = new ArrayList<>();
+	
+	ScoreTrakker(ArrayList<Student> list) {
+		
+	}
+	
+	
+	// loads in data file and stores student object into list
+	public void loadDataFile(String fileName) {
+		
+	}
+	
+	// sorts the array list and prints it
+	public void printInOrder() {
+		
+	}
+	
+	// calls loadDataFile and printInOrder for correct files
+	public void processFiles() {
+		loadDataFile("scores.txt");
+		printInOrder();
+	}
+	
+	//main
+	public static void main(String[] args) {
+		
+	}
+}
